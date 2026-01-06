a6b865c390da6aa37aa2f16d240527ecc75fdbe5
Anchorclaws <romain.trohel1811@gmail.com>
2026-01-06 18:16:54 +0100

    Update jokers2.lua

--- joker/jokers2.lua
+++ joker/jokers2.lua
@@ -1,19 +1,24 @@
 SMODS.Joker{
     key = "ryze",
-    
-
     rarity = 1,
-    cost = 6,
+    cost = 2,
     unlocked = true,
     discovered = true,
-
-    blueprint_compat = true,
+    blueprint_compat = false,
     eternal_compat = true,
     perishable_compat = true,
-
-    effect = "splash",
-
+    
+    -- Description et bulle Splash
     loc_vars = function(self, info_queue, card)
-      info_queue[#info_queue+1] = { set = 'Joker', key = 'j_splash', config={}}
+        info_queue[#info_queue+1] = { set = 'Joker', key = 'j_splash', config={}}
+    end,
+    
+    calculate = function(self, card, context)
+        if context.modify_scoring_hand and not context.blueprint then
+            return {
+                add_to_hand = true
+            }
+        end
     end
+
 }
